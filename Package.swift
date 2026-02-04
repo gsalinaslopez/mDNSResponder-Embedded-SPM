ff78ce3e235b955917730b1de583242169468d76
Giovanni Salinas <gsalinaslopez@gmail.com>
2026-02-04 08:27:33 +0800

    package header path cleanup

--- Package.swift
+++ Package.swift
@@ -21,45 +21,32 @@ let package = Package(
                 "./mDNSPosix/ExampleClientApp.c",
                 "./mDNSPosix/mDNSPosix.c",
                 "./mDNSPosix/mDNSUNP.c",
-                //"./mDNSPosix/Identify.c",
                 "./mDNSCore/DNSCommon.c",
                 "./mDNSCore/DNSDigest.c",
                 "./mDNSCore/mDNS.c",
                 "./mDNSCore/uDNS.c",
-                //"./mDNSShared/dnsextd.c",
                 "./mDNSShared/mDNSDebug.c",
                 "./mDNSShared/GenLinkedList.c",
                 "./mDNSShared/PlatformCommon.c",
-                /*
-                "./mDNSCore",
-                "./mDNSShared",
-                */
             ],
             cSettings: [
                 .headerSearchPath("./mDNSPosix/ExampleClientApp.h"),
                 .headerSearchPath("./mDNSPosix/mDNSPosix.h"),
                 .headerSearchPath("./mDNSCore/"),
                 .headerSearchPath("./mDNSShared/"),
-                //.headerSearchPath("./mDNSShared/dnsextd.h"),
                 .headerSearchPath("./mDNSShared/utilities/"),
-                //.headerSearchPath("./mDNSCore/mDNSEmbeddedAPI.h"),
-                /*
-                .headerSearchPath("./mDNSCore"),
-                .headerSearchPath("./mDNSShared"),
-                .headerSearchPath("./include")
-                */
                 .unsafeFlags([
                     "-D_GNU_SOURCE",
-                    "-DHAVE_IPV6",
+                    //"-DHAVE_IPV6",
                     "-DNOT_HAVE_SA_LEN",
                     "-DUSES_NETLINK",
                     "-DHAVE_LINUX",
-                    "-DTARGET_OS_LINUX",
-                    "-D_POSIX_HAS_TLS",
-                    "-ftabstop=4",
+                    //"-DTARGET_OS_LINUX",
+                    //"-D_POSIX_HAS_TLS",
+                    //"-ftabstop=4",
                     //"Wno-expansion-to-defined",
-                    "-g",
-                    "-DMDNS_DEBUGMSGS=0",
+                    //"-g",
+                    //"-DMDNS_DEBUGMSGS=0",
                 ])
             ]
         ),
